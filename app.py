a89c8a36c72dd85c22dfd819966d3be96ce1eb6f
Ted McCormack <tmack@Teds-Macbook.attlocal.net>
2016-09-07 01:34:08 -0700

    checkpoint

--- app.py
+++ app.py
@@ -42,24 +42,28 @@ def login_required(test):
 
 @app.route('/')
 def index():
-    return render_template('pages/placeholder.home.html')
-
+    return render_template('pages/home.html')
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
+    return render_template('pages/home.html')
 
+@app.route('/lc')
+def lc():
+    return render_template('pages/lc.html')
 
 @app.route('/about')
 def about():
-    return render_template('pages/placeholder.about.html')
+    return render_template('pages/about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('pages/contact.html')
 
 @app.route('/login')
 def login():
     form = LoginForm(request.form)
     return render_template('forms/login.html', form=form)
-
 
 @app.route('/register')
 def register():
