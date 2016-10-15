54b21dba8c729e9eb5e88f6fd5d1b4e317cdf059
Ted McCormack <uzbit@uzbit.net>
2016-10-15 10:30:25 -0700

    Updating plot

--- webapp/static/plots/roc_auc_var.div.js
+++ webapp/static/plots/roc_auc_var.div.js
@@ -1,1 +1,1 @@
-document.write(' <div class="bk-root">     <div class="plotdiv" id="db8eab96-2f8c-4e27-8a1f-7d199de817c4"></div> </div>');
+document.write(' <div class="bk-root">     <div class="plotdiv" id="c39f686f-9d36-468c-902f-b58bdece009c"></div> </div>');
