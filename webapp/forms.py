6adba6d811ede1713aec8a7c571f3248095d5198
Ted McCormack <uzbit@uzbit.net>
2016-11-20 15:06:58 -0800

    work on adding email, flashing now works on contact form

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,15 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length, Email
+from flask import flash
 
-# Set your classes here.
+def flash_errors(form):
+	import sys
+	for field, errors in form.errors.items():
+		print errors
+		sys.stdout.flush()
+		for error in errors:
+			flash(u"Error in the %s field - %s" % (
+				getattr(form, field).label.text,
+				error
+			))
 
 class RegisterForm(FlaskForm):
 	name = TextField(
 		'Username', validators=[DataRequired(), Length(min=6, max=25)]
 	)
 	email = TextField(
-		'Email', validators=[DataRequired(), Length(min=6, max=40)]
+		'Email', validators=[Email(), Length(min=6, max=40)]
 	)
 	password = PasswordField(
 		'Password', validators=[DataRequired(), Length(min=6, max=40)]
@@ -31,5 +41,5 @@ class ForgotForm(FlaskForm):
 
 class ContactForm(FlaskForm):
 	name = TextField('Name', [DataRequired()])
-	email = TextField('Email', validators=[DataRequired(), Length(min=0, max=100)])
+	email = TextField('Email', validators=[Email(), Length(min=0, max=100)])
 	comments = TextAreaField('Comments', [DataRequired()])
--- webapp/views/contact.py
+++ webapp/views/contact.py
@@ -3,8 +3,9 @@ import sendgrid
 import datetime
 from sendgrid.helpers.mail import *
 from config import SENDGRID_API_KEY
-from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect
 from webapp.forms import ContactForm
+from webapp.forms import flash_errors
 
 contact_blueprint = Blueprint('contact', __name__)
 
@@ -14,28 +15,29 @@ def index():
 
 	if form.validate_on_submit():
 		send_email(form.name.data, form.email.data, form.comments.data)
+
 		return render_template('pages/thankyou.html')
 
+	flash_errors(form)
 	return render_template('pages/contact.html', form=form)
 
 def _get_email(name, email, comments):
-	ret =  "------------------------------------------------------------"
-	ret += "\tRecieved feedback at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	ret += "\t%s (%s) writes: \n" % (name, email)
-	ret += "\t%s" % comments
-	ret += "------------------------------------------------------------"
-	return ret
+	ret = list()
+	ret += ["------------------------------------------------------------"]
+	ret += ["\tRecieved feedback at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+	ret += ["\t%s (%s) writes: \n" % (name, email)]
+	ret += ["\t%s" % comments]
+	ret += ["------------------------------------------------------------"]
+	return "<br>\n".join(ret)
 
 def send_email(name, email, comments):
 	email_body = _get_email(name, email, comments)
-
 	sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
 	from_email = Email(email)
 	subject = "[Machine Lending] - Contact"
 	to_email = Email("guzbit@gmail.com")
-	content = Content("text/plain", email_body)
+	content = Content("text/html", email_body)
 	mail = Mail(from_email, subject, to_email, content)
 	response = sg.client.mail.send.post(request_body=mail.get())
-	print(response.status_code)
-	print(response.body)
-	print(response.headers)
+	if response.status_code != 202:
+		print "Error sending email:\n%s" % email_body
