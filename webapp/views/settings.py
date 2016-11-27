ebfc8d1cf4d4803e3d76581ccbc855f53f93d27a
uzbit <uzbit@uzbit.net>
2016-11-26 19:56:56 -0800

    rework of the settings for account info handling

--- webapp/views/settings.py
+++ webapp/views/settings.py
@@ -14,6 +14,52 @@ settings_blueprint = flask.Blueprint('settings', __name__)
 def index():
 	return flask.render_template('layouts/settings.html')
 
+# def invalidate_lc_account_info():
+# 	account_info = UsersLCAccountInfo.get_by_user_id(
+# 		current_user.id
+# 	)
+# 	account_info.enc_api_key = None
+# 	account_info.enc_account_number = None
+# 	try:
+# 		account_info.commit()
+# 		flask.flash("Lending Club account credentials invalidated.", 'warning')
+# 		return flask.redirect(flask.url_for('settings.lc'))
+# 	except Exception as e:
+# 		flask.flash(str(e), 'danger')
+
+def get_lc_account_info():
+	account_info = UsersLCAccountInfo.get_by_user_id(
+		current_user.id
+	)
+	api_key = None
+	account_number = None
+	if account_info:
+		if account_info.enc_api_key:
+			api_key = decrypt_data(
+				account_info.enc_api_key,
+				current_user.enc_password
+			)
+		if account_info.enc_account_number:
+			account_number = decrypt_data(
+				account_info.enc_account_number,
+				current_user.enc_password
+			)
+	return account_info, api_key, account_number
+
+def update_lc_account_info(account_info, api_key, account_number):
+	if account_info:
+		account_info.enc_api_key = encrypt_data(
+			api_key, current_user.enc_password
+		)
+		account_info.enc_account_number = encrypt_data(
+			account_number, current_user.enc_password
+		)
+		try:
+			account_info.commit()
+			flask.flash("Saved LC account info.", 'success')
+		except Exception as e:
+			flask.flash(str(e), 'danger')
+
 @settings_blueprint.route('/settings/ml', methods=['get', 'post'])
 @login_required
 def ml():
@@ -23,10 +69,12 @@ def ml():
 	if form.validate_on_submit():
 		current_user.email = form.email.data
 		password = form.password.data.encode('utf-8')
-		if form.password.data:
+		# Note, need to invalidate the lc settings...
+		if password:
 			current_user.enc_password = bcrypt.hashpw(
 				password, bcrypt.gensalt()
 			)
+			update_lc_account_info(*get_lc_account_info())
 		try:
 			current_user.commit()
 			flask.flash("Update success!", 'success')
@@ -41,36 +89,23 @@ def ml():
 @login_required
 def lc():
 	form = LCAccountInfoForm(flask.request.form)
+	account_info, api_key, account_number = get_lc_account_info()
 	if flask.request.method == 'GET':
-		account_info = UsersLCAccountInfo.get_by_user_id(
-			current_user.id
-		)
-		if account_info:
-			#form.api_key.data = account_info.enc_api_key
-			form.api_key.data = decrypt_data(
-				account_info.enc_api_key,
-				current_user.enc_password
-			)
-			#form.account_number.data = account_info.enc_account_number
-			form.account_number.data = decrypt_data(
-				account_info.enc_account_number,
-				current_user.enc_password
-			)
+		form.api_key.data = api_key
+		form.account_number.data = account_number
 	else:
 		if form.validate_on_submit():
-			api_key = form.api_key.data
-			account_number = form.account_number.data
-			new_account_info = UsersLCAccountInfo(
-				current_user,
-				encrypt_data(api_key, current_user.enc_password),
-				encrypt_data(account_number, current_user.enc_password)
-			)
-			try:
-				new_account_info.commit()
-				flask.flash("Saved LC account info.", 'success')
-				return flask.redirect(flask.url_for('settings.lc'))
-			except Exception as e:
-				flask.flash(str(e), 'danger')
+			if account_info:
+				update_lc_account_info(account_info, api_key, account_number)
+			else:
+				api_key = form.api_key.data
+				account_number = form.account_number.data
+				account_info = UsersLCAccountInfo(
+					current_user,
+					bytes(),
+					bytes()
+				)
+				update_lc_account_info(account_info, api_key, account_number)
 
 	flash_errors(form)
 	return flask.render_template('pages/lc_account.html', form=form)
