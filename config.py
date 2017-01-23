7510d55842dca3ca0a831a3665af7f19c51784c6
uzbit <uzbit@uzbit.net>
2017-01-22 18:37:46 -0800

    Wrong key name

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ BASE_DIR = basedir
 
 # Enable debug mode.
 DEBUG = True
+TEST = True
 
 # Secret key for session management. You can generate random strings here:
 # http://clsc.net/tools-old/random-string-generator.php
