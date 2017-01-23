115fce528e1c47c8d4a4db61206d91c37017931b
uzbit <uzbit@uzbit.net>
2017-01-22 16:58:03 -0800

    Adding recaptcha

--- webapp/views/register.py
+++ webapp/views/register.py
@@ -18,6 +18,7 @@ def index():
 			bcrypt.hashpw(password, bcrypt.gensalt()),
 			form.email.data
 		)
+		print_log(form.recaptcha.data)
 		try:
 			new_user.commit()
 			login_user(new_user, remember=True)
