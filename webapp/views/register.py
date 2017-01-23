111bdfd9ecb3490116c08a081c9f8c8da4688df2
uzbit <uzbit@uzbit.net>
2017-01-22 16:03:57 -0800

    log capta

--- webapp/views/register.py
+++ webapp/views/register.py
@@ -10,6 +10,7 @@ register_blueprint = flask.Blueprint('register', __name__)
 @register_blueprint.route('/register', methods=['get', 'post'])
 def index():
 	form = RegisterForm(flask.request.form)
+	print_log(flask.request)
 	if form.validate_on_submit():
 		password = form.password.data.encode('utf-8')
 		new_user = User(
