a72e7469bfb7b6c5a0a41a187b0c86e70b1e35ed
uzbit <uzbit@uzbit.net>
2017-01-22 17:39:51 -0800

    Message category

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -19,6 +19,7 @@ app.config.from_object('config')
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login.index'
+login_manager.login_message_category = 'danger'
 
 #@app.before_request
 #def before_request():
